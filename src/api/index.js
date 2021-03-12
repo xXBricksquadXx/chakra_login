42b87d34fa1b7b9a39544dc2e66fb8d54114b6ef
colby <colby88cardell@gmail.com>
2021-03-11 20:45:31 -0500

    :construction: Setting up api with Firebase auth

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+import auth from './auth';
+
+const api = {
+  show(email, password) {
+    console.log(auth);
+  },
+
+  create(email, password) {
+    console.log(auth);
+  },
+
+  update(email) {
+    console.log(auth);
+  },
+};
+
+export default api;
