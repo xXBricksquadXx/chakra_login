0bb9d6911c7c7cc785e0a83fe5728ee954c6b068
colby <colby88cardell@gmail.com>
2021-03-11 20:29:50 -0500

    :sparkles: Add transitioning Login/reg form

--- src/components/LoginRegistrationForm/index.js
+++ src/components/LoginRegistrationForm/index.js
@@ -1,12 +1,21 @@
 import {
   Button,
   ButtonGroup,
+  Collapse,
+  Fade,
   FormControl,
   FormLabel,
   Input,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 function LoginRegistrationForm() {
+  const [forgot, setForgot] = useState(false);
+
+  const handleClick = () => {
+    setForgot(prev => !prev);
+  };
+
   return (
     <form className="flex flex-col gap-4">
       <FormControl id="email">
@@ -14,15 +23,25 @@ function LoginRegistrationForm() {
         <Input type="email" />
       </FormControl>
 
-      <FormControl id="password">
-        <FormLabel>Password</FormLabel>
-        <Input type="password" />
-      </FormControl>
+      <Collapse in={!forgot} animateOpacity>
+        <FormControl id="password">
+          <FormLabel>Password</FormLabel>
+          <Input type="password" />
+        </FormControl>
+      </Collapse>
 
       <ButtonGroup variant="outline" spacing="6">
-        <Button colorScheme="green">Login</Button>
-        <Button colorScheme="blue">Register</Button>
-        <Button colorScheme="orange">Forgot Password?</Button>
+        <Button colorScheme="green">
+          {forgot ? 'Reset Password' : 'Login'}
+        </Button>
+
+        <Fade in={!forgot} animateOpacity>
+          <Button colorScheme="blue">Register</Button>
+        </Fade>
+
+        <Button colorScheme="orange" onClick={handleClick}>
+          {forgot ? 'Login/Register' : 'Forgot Password?'}
+        </Button>
       </ButtonGroup>
     </form>
   );
