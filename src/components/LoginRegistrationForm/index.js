3e1e018644011f78586f3179a2dd0218d801ccdf
colby <colby88cardell@gmail.com>
2021-03-11 20:47:33 -0500

    :construction: Configure submission buttons

--- src/components/LoginRegistrationForm/index.js
+++ src/components/LoginRegistrationForm/index.js
@@ -16,8 +16,18 @@ function LoginRegistrationForm() {
     setForgot(prev => !prev);
   };
 
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    /**
+     * TODOs: 1. Identify which button was clicked from its `innerText` so we know which api method to trigger.
+     */
+
+    console.log(event, 'xxx');
+  };
+
   return (
-    <form className="flex flex-col gap-4">
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
       <FormControl id="email">
         <FormLabel>Email address</FormLabel>
         <Input type="email" />
@@ -31,15 +41,17 @@ function LoginRegistrationForm() {
       </Collapse>
 
       <ButtonGroup variant="outline" spacing="6">
-        <Button colorScheme="green">
+        <Button type="submit" colorScheme="green">
           {forgot ? 'Reset Password' : 'Login'}
         </Button>
 
         <Fade in={!forgot} animateOpacity>
-          <Button colorScheme="blue">Register</Button>
+          <Button type="submit" colorScheme="blue">
+            Register
+          </Button>
         </Fade>
 
-        <Button colorScheme="orange" onClick={handleClick}>
+        <Button type="button" colorScheme="orange" onClick={handleClick}>
           {forgot ? 'Login/Register' : 'Forgot Password?'}
         </Button>
       </ButtonGroup>
