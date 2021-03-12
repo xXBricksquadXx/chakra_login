df67d4d5d51cba86c7e2b1edf99f29003c64f9fa
colby <colby88cardell@gmail.com>
2021-03-12 13:43:33 -0500

    :construction: Added config key

--- src/api/auth/config.js
+++ src/api/auth/config.js
@@ -1,3 +1,10 @@
-const config = {};
+const config = {
+  apiKey: 'AIzaSyANESAb0px8Yn3NKvalGRCspoKqfKBPaFE',
+  authDomain: 'login-auth-bd5d0.firebaseapp.com',
+  projectId: 'login-auth-bd5d0',
+  storageBucket: 'login-auth-bd5d0.appspot.com',
+  messagingSenderId: '573634852138',
+  appId: '1:573634852138:web:ed6f4750b090fae48e5a53',
+};
 
 export default config;
